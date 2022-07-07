11f33b83000c2a5663135441432e23bf9306d811
Alexander Pototsky <silencemyalise@gmail.com>
2022-07-07 17:04:58 +0600

    add model TriangleModel

---
+++ ShapeLibrary/Formulas.cs
@@ -0,0 +1,18 @@
+﻿using ShapeLibrary.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeLibrary
+{
+    public class Formulas
+    {
+        public double CalculatingArea(TriangleModel obj)
+        {
+            double p = obj.A + obj.B + obj.C;
+            return Math.Sqrt(p / 2 * (p / 2 - obj.A) * (p / 2 - obj.B) * (p / 2 - obj.C));
+        }
+    }
+}
---
+++ ShapeLibrary/Model/TriangleModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeLibrary.Model
+{
+    public class TriangleModel
+    {
+        public int A { get; }
+        public int B { get; }
+        public int C { get; }
+        public bool RectangularTriangle { get; set; }
+
+        public TriangleModel(int a, int b, int c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+    }
+}
